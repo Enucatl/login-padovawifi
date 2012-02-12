ee758bb20ce274479c3a14f1a09b628a8dc6dc00
Matteo Abis <gmatteo.abis@gmail.com>
2012-02-13 00:43:35 +0100

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+from __future__ import print_function
+import sys
+
+print("Hi! Welcome to login-padovawifi.")
+print("I'm going to check if you installed the mechanize module...")
+try:
+    import mechanize
+    print("mechanize found (OK!)")
+except ImportError:
+    print("""could not find the mechanize module. Please install it.
+            If you are running an ubuntu system, just type:
+            
+            sudo apt-get install python-mechanize""")
+    sys.exit(1)
+
+print("""Beware! The password is going to be saved as plain text. Encryption
+        is not yet supported. Do you want to continue? """, end='')
+cont = raw_input("[S/n] ")
+if cont is "n":
+    sys.exit(1)
+
+username = raw_input("Enter your padovawifi username: ")
+password = raw_input("Enter your padovawifi password: ")
+
+file_name = "padovawifi_password"
+with open(file_name, 'w') as output_file:
+    output_file.write(username)
+    output_file.write('\n')
+    output_file.write(password)
+    output_file.write('\n')
